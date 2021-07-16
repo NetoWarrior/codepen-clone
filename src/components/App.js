fdcf5b4c0b50a672c82c72e64fc6c105d6e195d1
Fedrick Neto <fedrickneto@Fedricks-Air.mshome.net>
2021-07-16 12:50:08 +0530

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,96 @@
+import React, { useState, useEffect } from 'react';
+import Editor from './Editor'
+import useLocalStorage from '../hooks/useLocalStorage'
+
+function App() {
+  const [html, setHtml] = useLocalStorage('html', '')
+  const [css, setCss] = useLocalStorage('css', '')
+  const [js, setJs] = useLocalStorage('js', '')
+  const [srcDoc, setSrcDoc] = useState('')
+
+
+// State to manage file system
+  const[showHTMLEditor,setShowHTMLEditor] = React.useState(true);
+  const[showCSSEditor,setShowCSSEditor] = React.useState(false);
+  const[showJSEditor,setShowJSEditor] = React.useState(false);
+
+  const onClickHTML = () => {
+    setShowHTMLEditor(!showHTMLEditor);
+    (showCSSEditor) && setShowCSSEditor(!showCSSEditor) ; 
+    (showJSEditor) && setShowJSEditor(!showJSEditor) ;
+  }
+  const onClickCSS = () => {
+    setShowCSSEditor(!showCSSEditor);
+    (showJSEditor) && setShowJSEditor(!showJSEditor);
+    (showHTMLEditor) && setShowHTMLEditor(!showHTMLEditor);
+  }
+  const onClickJS = () => {
+    setShowJSEditor(!showJSEditor);
+    (showCSSEditor) && setShowCSSEditor(!showCSSEditor) ; 
+    (showHTMLEditor) && setShowHTMLEditor(!showHTMLEditor);
+  }
+
+  // Save File on localStorage every few seconds
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setSrcDoc(`
+        <html>
+          <body>${html}</body>
+          <style>${css}</style>
+          <script>${js}</script>
+        </html>
+      `)
+    }, 250)
+
+    return () => clearTimeout(timeout)
+  }, [html, css, js])
+
+  return (
+    <>
+    
+      {/*Editor Section and File Explorer */}
+      <div className="pane top-pane">
+        <div classname="file-explorer"> 
+        <input type="submit" value="Index.html" onClick={onClickHTML} />
+        <input type="submit" value="Index.css" onClick={onClickCSS} />
+        <input type="submit" value="Index.JS" onClick={onClickJS} />
+        </div>
+       {/* Html Editor */}
+       {showHTMLEditor && <Editor
+          language="xml"
+          displayName="index.html"
+          value={html}
+          onChange={setHtml}
+        />}
+        {/* CSS Editor */}
+        {showCSSEditor && <Editor
+          language="css"
+          displayName="index.css"
+          value={css}
+          onChange={setCss}
+        />}
+        {/* JS Editor */}
+        {showJSEditor && <Editor
+          language="javascript"
+          displayName="index.js"
+          value={js}
+          onChange={setJs}
+        />}
+      </div>
+
+      {/* Live View Section */}
+      <div className="pane">
+        <iframe
+          srcDoc={srcDoc}
+          title="output"
+          sandbox="allow-scripts"
+          frameBorder="0"
+          width="100%"
+          height="100%"
+        />
+      </div>
+    </>
+  )
+}
+
+export default App;
