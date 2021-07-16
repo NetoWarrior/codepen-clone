7837d4d9dadd2f2f9471ef8d9775a014df95e2c6
Fedrick Neto <fedrickneto@Fedricks-Air.mshome.net>
2021-07-16 13:08:36 +0530

    Updates

---
+++ precache-manifest.b8d474314eda959c1eb23a083853dc6e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0b3ac4fa999de40788556ae8caa8157",
+    "url": "/codepen-clone/index.html"
+  },
+  {
+    "revision": "cd06432170f12ce58161",
+    "url": "/codepen-clone/static/css/2.c46397a6.chunk.css"
+  },
+  {
+    "revision": "7742e4baaa891c118507",
+    "url": "/codepen-clone/static/css/main.72420445.chunk.css"
+  },
+  {
+    "revision": "cd06432170f12ce58161",
+    "url": "/codepen-clone/static/js/2.7c81a192.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/codepen-clone/static/js/2.7c81a192.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7742e4baaa891c118507",
+    "url": "/codepen-clone/static/js/main.3659a94e.chunk.js"
+  },
+  {
+    "revision": "7e95b33d3c3f8b32c9c4",
+    "url": "/codepen-clone/static/js/runtime-main.3a6f25c4.js"
+  }
+]);
